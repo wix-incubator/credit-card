1f766aa526c34098bc4cece5bac6ef54ef3123f7
Danny Leshem <dleshem@gmail.com>
2015-06-15 09:49:49 +0300

    Fixed broken documentation link.

--- src/main/scala/com/wix/pay/creditcard/InvalidCreditCardNumberException.scala
+++ src/main/scala/com/wix/pay/creditcard/InvalidCreditCardNumberException.scala
@@ -7,7 +7,7 @@
 package com.wix.pay.creditcard
 
 
-/** An [[Exception]] that indicates that a Credit Card number is invalid (e.g., does not pass ''Luhn'' validation,
+/** Indicates that a Credit Card number is invalid (e.g., does not pass ''Luhn'' validation,
   * or ''Isracard'' validation, in the case of ''Isracard'' credit cards).
   *
   * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
