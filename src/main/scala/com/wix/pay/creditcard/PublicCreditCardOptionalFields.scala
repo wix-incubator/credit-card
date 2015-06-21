01bde9603d1e3be50858bb7e470c8ad63c568554
Danny Leshem <dleshem@gmail.com>
2015-06-21 17:19:48 +0300

    PublicCreditCard and PublicCreditCardOptionalFields fields are now annotated with @BeanProperty, for easier interoperability with Java (e.g. implicit JSON serialization).

--- src/main/scala/com/wix/pay/creditcard/PublicCreditCardOptionalFields.scala
+++ src/main/scala/com/wix/pay/creditcard/PublicCreditCardOptionalFields.scala
@@ -6,13 +6,15 @@
 \*                |/                                                 */
 package com.wix.pay.creditcard
 
+import scala.beans.BeanProperty
+
 
 /** Container that holds optional fields of a Public Credit Card (e.g., holder ID, billing address).
   *
   * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
   */
-case class PublicCreditCardOptionalFields(override val holderId: Option[String] = None,
-                                          override val holderName: Option[String] = None,
-                                          override val billingAddress: Option[String] = None,
-                                          override val billingPostalCode: Option[String] = None)
+case class PublicCreditCardOptionalFields(@BeanProperty override val holderId: Option[String] = None,
+                                          @BeanProperty override val holderName: Option[String] = None,
+                                          @BeanProperty override val billingAddress: Option[String] = None,
+                                          @BeanProperty override val billingPostalCode: Option[String] = None)
   extends Serializable with CommonPublicCreditCardFields
